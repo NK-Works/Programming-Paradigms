903f0e021fe4ed7be40becf3764c24a09e5fdbd1
NK-Works <132702983+NK-Works@users.noreply.github.com>
2024-02-20 12:41:44 +0530

    Add files via upload

---
+++ M2-Concurrent Programming/Task2.1P/m2_t1p_opm.cpp
@@ -0,0 +1,101 @@
+/* This code is made by Anneshu Nag, Student ID-2210994760*/
+/*     This is a OPM parallel matrix multiplication.      */
+
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <omp.h>
+#include <chrono>
+
+using namespace std;
+using namespace chrono;
+
+int main() {
+    int matrixSize;
+
+    // Getting the size of the matrix from the user and ensure it is between 2 and 12
+    do {
+        cout << "Enter the size of the matrix (between 2 and 12): ";
+        cin >> matrixSize;
+
+        if (matrixSize < 2 || matrixSize > 12) {
+            cout << "Invalid size. Please enter a size between 2 and 12." << endl;
+        }
+
+    } while (matrixSize < 2 || matrixSize > 12);
+
+    int matrixA[matrixSize][matrixSize], matrixB[matrixSize][matrixSize], matrixOut[matrixSize][matrixSize]; // A,B,C matrix
+
+    // Generating a random matrixSizeber
+    random_device random;
+    mt19937 genRand(random());
+    uniform_int_distribution<> dist(0, 100);
+
+    // initializing matrices A and B with random values
+    for (int i = 0; i < matrixSize; ++i) {
+        for (int j = 0; j < matrixSize; ++j) {
+            matrixA[i][j] = dist(genRand);
+            matrixB[i][j] = dist(genRand);
+        }
+    }
+
+    // Start the timer
+    auto start_time = high_resolution_clock::now();
+
+    // Performing matrix multiplication in parallel
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i < matrixSize; ++i) {
+        for (int j = 0; j < matrixSize; ++j) {
+            matrixOut[i][j] = 0;
+            for (int k = 0; k < matrixSize; ++k) {
+                matrixOut[i][j] += matrixA[i][k] * matrixB[k][j];
+            }
+        }
+    }
+
+    // Stop the timer
+    auto end_time = high_resolution_clock::now();
+
+    // Calculate and print the elapsed time
+    auto duration = duration_cast<microseconds>(end_time - start_time);
+    cout << "Time taken for matrix multiplication: " << duration.count() << " microseconds." << endl;
+
+    // Printing the result to the console
+    cout << "::: Resulting Matrix :::" << endl;
+      // Printing the result to the console
+    for (int i = 0; i < matrixSize; ++i)
+    {
+        for (int j = 0; j < matrixSize; ++j)
+        {
+            cout << matrixOut[i][j] << " ";
+        }
+        cout << endl;
+    }
+    // Open a text file for writing
+    ofstream outputFile("opm_output.txt");
+
+    if (outputFile.is_open()) {
+        // Redirect the output to the file
+        streambuf *coutbuf = cout.rdbuf();
+        cout.rdbuf(outputFile.rdbuf());
+
+        for (int i = 0; i < matrixSize; ++i) {
+            for (int j = 0; j < matrixSize; ++j) {
+                cout << matrixOut[i][j] << " ";
+            }
+            cout << endl;
+        }
+
+        // Close the file
+        outputFile.close();
+
+        // Restore cout back to the console
+        cout.rdbuf(coutbuf);
+
+        cout << "\nOutput has been written to 'opm_output.txt'" << endl;
+    } else {
+        cerr << "\nUnable to open output.txt for writing." << endl;
+    }
+
+    return 0;
+}
